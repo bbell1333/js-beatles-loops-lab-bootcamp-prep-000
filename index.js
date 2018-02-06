b98db8fe80e86f672856400d60c732ea53b75188
Bryan Bell <bbell1333@gmail.com>
2018-02-06 20:08:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments){
   var beatles = [];
   for (var i = 0; i<4;i++){
-    beatles.push(`${musicians[i]} plays ${instruments[i]
+    beatles.push(`${musicians[i]} plays ${instruments[i]'
   
   return beatles;
 }
