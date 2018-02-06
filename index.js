be9c433ee4cae3a329f9d69d2357aad7bc8ac8e7
Bryan Bell <bbell1333@gmail.com>
2018-02-06 19:58:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function theBeatlesPlay(musicians, instruments){
   var beatles = [];
-  var musicians = ["John Lennon","Paul McCartney","George Harrison", "Ringo Starr"];
+  musicians = ["John Lennon","Paul McCartney","George Harrison", "Ringo Starr"];
 }
